058990b8d787a6e5825aadc76ac2f2d0b420602e
Mehmet Eski <157639357+mehmeteski-42@users.noreply.github.com>
2025-04-10 23:32:30 +0300

    Update hive_test.py

--- hive_test.py
+++ hive_test.py
@@ -1,25 +1,55 @@
 from pyspark.sql import SparkSession
 import time
+import os
 
 spark = SparkSession.builder \
-    .appName("DataComparerWithHive") \
-    .master("local[*]") \
+    .appName("DataComparerHiveTest") \
     .enableHiveSupport() \
     .getOrCreate()
 
+DATA_DIR = "datasets"
 
-json_path = "datasets/sample.json"
+files = [
+    "data.avro",
+    "data.json",
+    "data.parquet",
+    "data.xml",
+]
 
-start = time.time()
+def read_file(file_path, ext):
+    if ext == ".json":
+        return spark.read.json(file_path)
+    elif ext == ".parquet":
+        return spark.read.parquet(file_path)
+    elif ext == ".avro":
+        return spark.read.format("avro").load(file_path)
+    elif ext == ".xml":
+        return spark.read.format("xml").option("rowTag", "root").load(file_path)  # rowTag ihtiyaca göre değişir
+    else:
+        raise Exception(f"Desteklenmeyen format: {ext}")
 
-df = spark.read.json(json_path)
+for file in files:
+    file_path = os.path.join(DATA_DIR, file)
+    name = os.path.splitext(file)[0]
+    ext = os.path.splitext(file)[1]
 
-df.write.mode("overwrite").saveAsTable("json_table_test")
+    print(f"\n{file} dosyası işleniyor...")
 
-result = spark.sql("SELECT * FROM json_table_test LIMIT 10")
-result.show()
+    try:
+        start = time.time()
 
-end = time.time()
-print(f"İşlem süresi: {end - start:.2f} saniye")
+        df = read_file(file_path, ext)
+        df.printSchema()
+
+        table_name = f"hive_{name}"
+        df.write.mode("overwrite").saveAsTable(table_name)
+
+        spark.sql(f"SELECT * FROM {table_name} LIMIT 10").show()
+
+        end = time.time()
+        print(f"{file} başarıyla işlendi. Süre: {end - start:.2f} saniye")
+
+    except Exception as e:
+        print(f"{file} dosyası işlenirken hata oluştu: {e}")
 
 spark.stop()
