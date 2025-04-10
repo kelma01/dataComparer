1c44f2bacb9023b1a9134a3c791f06e8d392c2c7
Mehmet Eski <157639357+mehmeteski-42@users.noreply.github.com>
2025-04-10 19:05:43 +0300

    Update hive_test.py

--- hive_test.py
+++ hive_test.py
@@ -1,26 +1,21 @@
 from pyspark.sql import SparkSession
 import time
 
-# SparkSession - Hive destekli
 spark = SparkSession.builder \
     .appName("DataComparerWithHive") \
     .master("local[*]") \
     .enableHiveSupport() \
     .getOrCreate()
 
-# JSON dosyasının yolu
-json_path = "data/sample.json"  # Dosyayı buraya koyabilirsin
 
-# Süreyi ölç
+json_path = "datasets/sample.json"
+
 start = time.time()
 
-# JSON verisini oku
 df = spark.read.json(json_path)
 
-# Hive tablosu olarak kaydet
 df.write.mode("overwrite").saveAsTable("json_table_test")
 
-# Sorgu örneği
 result = spark.sql("SELECT * FROM json_table_test LIMIT 10")
 result.show()
 
